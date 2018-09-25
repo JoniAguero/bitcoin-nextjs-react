3f2eafd4458198187c1074d99fe15ee12ba3f187
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-25 18:10:40 -0300

    Get Precio Bitcoint
    Se utiliza isomorphic-unfetch para hacer peticiones get en ssr.

--- component/Layout.js
+++ component/Layout.js
@@ -7,10 +7,14 @@ const Layout = (props) => (
             <title>Bitcoin App</title>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css" />
+            {/* <link rel="stylesheet" href="./css/index.css" /> */}
         </Head>
         <Navegacion />
         {/* Imprime lo que hay dentro del Layout */}
-        {props.children}
+        <div className="container">
+            {props.children}
+        </div>
+        
     </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
-import Layout from "../component/Layout";
+import Layout from '../component/Layout';
+import fetch  from 'isomorphic-unfetch';
+import Precio from '../component/Precio';
 
-const Index = () => (
+const Index = (props) => (
     <Layout>
-        <div>   
-            <p>Hola Inicio</p>
+        <div className="row">   
+            <div className="col-12">
+                <Precio precio={props.bitcoin} />
+            </div>
+            <div className="col-md-8">
+                <h2>Noticias sobre Bitcoin</h2>
+            </div>
+            <div className="col-md-4">
+                <h2>Eventos sobre Bitcoin</h2>
+            </div>
         </div>
     </Layout>
     
-)
+);
+Index.getInitialProps = async () => {
+    const precio = await fetch('https://api.coinmarketcap.com/v2/ticker/1');
+    const res = await precio.json();
+    return {
+        bitcoin: res.data.quotes.USD
+    }
+}
 
 export default Index;
