db72cf510bf15269ced3981262f73f5ce9a0e9df
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-25 10:00:05 -0300

    Agregando CSS a un Componente

--- pages/navegacion.js
+++ pages/navegacion.js
@@ -2,8 +2,26 @@ import Link from 'next/link';
 
 const Navegacion = () => (
     <div>
-        <li><Link href="/"><a>Inicio</a></Link></li>
-        <li><Link href="/nosotros"><a>Nosotros</a></Link></li>
+        <ul>
+            <li><Link href="/"><a>Inicio</a></Link></li>
+            <li><Link href="/nosotros"><a>Nosotros</a></Link></li>
+        </ul>
+        <style jsx>{`
+            ul {
+                background-color: #aaa;
+                list-style: none;
+                display: flex;
+            }
+            ul li {
+                padding: .5rem 0;
+                margin-right: .5rem;
+            }
+            ul li a {
+                font-size: 1.2rem;
+                color: white;
+                text-decoration: none;
+            }
+        `}</style>
     </div>
 )
 
